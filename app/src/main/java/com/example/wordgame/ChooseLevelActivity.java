9d674d74cccef5a9e94314028b408f50d423ba31
pceu <developer.aceu@gmail.com>
2019-03-30 17:52:31 +1100

    Add Level 1,2 and 3 activity

--- app/src/main/java/com/example/wordgame/ChooseLevelActivity.java
+++ app/src/main/java/com/example/wordgame/ChooseLevelActivity.java
@@ -1,13 +1,35 @@
 package com.example.wordgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseLevelActivity extends AppCompatActivity {
+
+    Button levelOneButton;
+    Button levelTwoButton;
+    Button levelThreeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_level);
+
+        levelOneButton = (Button) findViewById(R.id.levelOneButton);
+        levelTwoButton = (Button) findViewById(R.id.levelTwoButton);
+        levelThreeButton = (Button) findViewById(R.id.levelThreeButton);
     }
+
+    public void ToLevelOnePage(View view) {
+        Intent intent = new Intent (this, LevelOneActivity.class);
+        startActivity(intent);
+    }
+
+    public void ClickLevelOne(View view) {
+        ToLevelOnePage(view);
+    }
+
+
 }
--- app/src/main/java/com/example/wordgame/LevelOneActivity.java
+++ app/src/main/java/com/example/wordgame/LevelOneActivity.java
@@ -3,11 +3,31 @@ package com.example.wordgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LevelOneActivity extends AppCompatActivity {
+
+    private List<Level> testQuestion = new ArrayList<Level>();
+
+    private void addLevel() {
+        testQuestion.add(new Level(1, "Something that people do or cause to happen", "Act", "C", 1));
+        testQuestion.add(new Level(2, "Have Some ambitious plan", "Aim", "m", 1));
+        testQuestion.add(new Level(3, "Make a request or demand something", "ask", "k", 1));
+        testQuestion.add(new Level(1, "Not working properly", "Bad", "d", 1));
+    }
+
+    /*
+            HARD CODED QUESTIONS FOR LEVEL ONE (this will be modified when Level is stored in local drive)
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
+
+        // create a list of Level object to be used in this activity (LevelOne)
+        addLevel();
+
     }
 }
