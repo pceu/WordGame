d4318cd7b645129d8c684c79f230c474ffeb9d3b
pceu <developer.aceu@gmail.com>
2019-04-05 13:40:51 +1100

    - add button images

--- app/src/main/java/com/example/wordgame/LevelTwoActivity.java
+++ app/src/main/java/com/example/wordgame/LevelTwoActivity.java
@@ -3,8 +3,16 @@ package com.example.wordgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LevelTwoActivity extends AppCompatActivity {
 
+    private List<Level> levelTwoQ = new ArrayList<Level>();
+
+    private void loadLevel2() {
+        // to be added
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
