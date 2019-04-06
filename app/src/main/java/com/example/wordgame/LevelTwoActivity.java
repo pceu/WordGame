578f424ab73b8a66b51ae81116a42f09e7af168b
pceu <developer.aceu@gmail.com>
2019-04-06 16:53:07 +1100

    Work on Level 2 Activity layout (add required buttons and textView)

--- app/src/main/java/com/example/wordgame/LevelTwoActivity.java
+++ app/src/main/java/com/example/wordgame/LevelTwoActivity.java
@@ -8,14 +8,19 @@ import java.util.List;
 
 public class LevelTwoActivity extends AppCompatActivity {
 
-    private List<Level> levelTwoQ = new ArrayList<Level>();
+    private List<Level> levelTwoQuestion = new ArrayList<Level>();
 
     private void loadLevel2() {
-        // to be added
+        levelTwoQuestion.add(new Level(1, "When you know your mistake, you tend do this", "admit", "it", 2));
+        levelTwoQuestion.add(new Level(2, "Pain cause by involuntary contraction", "Cramp", "ap", 2));
+        levelTwoQuestion.add(new Level(2, "To ___ something on the surface", "apply", "py", 2));
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_two);
+
+        // instantiate level 2 object - contains question num, question, answer, hint and level number
+        loadLevel2();
     }
 }
