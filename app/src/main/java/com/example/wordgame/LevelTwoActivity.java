6b19ef9808836f5a9a983f1ca9ee4585fb93e556
pceu <developer.aceu@gmail.com>
2019-04-07 16:42:22 +1000

    work on Level 2 Activity  - add some codes to level2 class to load questions from Level Object  - modify level2 activity layout

--- app/src/main/java/com/example/wordgame/LevelTwoActivity.java
+++ app/src/main/java/com/example/wordgame/LevelTwoActivity.java
@@ -8,7 +8,10 @@ import java.util.List;
 
 public class LevelTwoActivity extends AppCompatActivity {
 
+    // Level object list for this level (Level 2) - contains multiple questions to play for the level
     private List<Level> levelTwoQuestion = new ArrayList<Level>();
+
+    private int userQuestionNumber;     // when exit the game the question user up to will be saved for user and allow to resume for future playing
 
     private void loadLevel2() {
         levelTwoQuestion.add(new Level(1, "When you know your mistake, you tend do this", "admit", "it", 2));
@@ -20,7 +23,20 @@ public class LevelTwoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_two);
 
+        userQuestionNumber = 1; // temporarily set as 1, but modified when User class is created and loaded here
         // instantiate level 2 object - contains question num, question, answer, hint and level number
         loadLevel2();
+
+        // for clarity purpose, a playLevelTwo method is created and call from here
+        playLevelTwo();
+
+    }
+
+    public void playLevelTwo() {
+        for (int i = userQuestionNumber; i <= levelTwoQuestion.size(); i++) {
+            // will add some codes later
+            return;
+        }
+
     }
 }
