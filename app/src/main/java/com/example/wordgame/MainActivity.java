8e7457d823f5d7e6dd75c438e4cf5e455c93eb6b
KimNamYoun <chohyun666@gmail.com>
2019-04-19 16:00:08 +1000

    Made Setting page

--- app/src/main/java/com/example/wordgame/MainActivity.java
+++ app/src/main/java/com/example/wordgame/MainActivity.java
@@ -17,4 +17,9 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, ChooseLevelActivity.class);
         startActivity(i);
     }
+
+    public void GotoSetting(View view) {
+        Intent i = new Intent(this, SettingActivity.class);
+        startActivity(i);
+    }
 }
